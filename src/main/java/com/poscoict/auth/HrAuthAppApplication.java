8e177d1fd9861a7137bb72558624b58688889b17
goaldae <goaldae@naver.com>
2022-01-11 19:40:53 +0900

    first commit

---
+++ src/main/java/com/poscoict/auth/HrAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.poscoict.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAuthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pack01/HrAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pack01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
